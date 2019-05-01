856807a8b2d22843fa26d8f885d744bf41c6231a
Garcia86 <46945202+Garcia86@users.noreply.github.com>
2019-04-30 21:43:53 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,21 +3,99 @@
 
 using namespace std;
 
-const int s = 27;
+const int s = 1;
 double grades[s];
 string names[s];
 bool run = true, loop = true;
+enum Grade { A = 90, B = 80, C = 70, D = 60, F = 50 };
+string letter;
+
+class Teacher{
+private:
+    string userName, password, inputUserName, inputPassword;
+    bool correctLogin;
+public:
+    Teacher(){
+        correctLogin = false;
+    }
+    bool isLoginCorrect(){
+        return correctLogin;
+    }
+    void setUserName(string teacherUserName){
+        userName = teacherUserName;
+    }
+
+    void setPassword(string teacherPassword){
+        password = teacherPassword;
+    }
+
+    string getUserName(){
+        return userName;
+    }
+
+    string getPassword(){
+        return password;
+    }
+    void login(string matchUsername, string matchPassword){
+        cout<< "Please enter user name: ";
+        cin >> inputUserName;
+        cout << "Please enter password: ";
+        cin >> inputPassword;
+        if(inputUserName == matchUsername && inputPassword == matchPassword){
+            correctLogin = true;
+        }else{
+            correctLogin = false;
+            cout << "Either the username or password was incorrect." << endl;
+            login(getUserName(), getPassword());
+        }
+    }
+
+};
 
 
-/**
- * The stats function takes the grades input for the classroom and will return the class average as well as the
- * highest and lowest grades in the class
- * @param sum - the total of all grades
- * @param average - the average of the grades
- * @param highest - the double which holds the value for the highest grade in the class
- * @param lowest - the double which holds the lowest grade in the class
- * @param grades - the array that holds the class grades
- */
+class Student {
+private:
+    string name, firstName, lastName;
+    int grade;
+public:
+    Student(){
+    }
+    
+    void setFirstName(string nameOne){
+        firstName = nameOne;
+    }
+
+    void setLastName(string nameTwo){
+        lastName = nameTwo;
+    }
+
+    string setName(){
+        name = firstName + " " + lastName;
+        return name;
+    }
+
+    void setGrade(int studentGrade){
+        grade = studentGrade;
+    }
+
+    string getName(){
+        return name;
+    }
+
+    string getFirstName(){
+        return firstName;
+    }
+
+    string getLastName() {
+        return lastName;
+    }
+
+    double getGrade(){
+        return grade;
+    }
+};
+
+
 void stats(){
     double sum=0.0, average, highest=0, lowest=101;
     for (int a = 0; a < s; a++) {
@@ -34,13 +112,6 @@ void stats(){
     cout<<endl;
 }
 
-/**
- * The grade finder function uses search strings to search for and find a student by name
- * @param names - the array in which the names of the students in the classroom are stored
- * @param grades - the array in which the students grades are stored
- * @param SearchCheck - a boolean used to check whether or not a match for the name searched has been found
- * @param SearchName - the user input to be searched for in the array names
- */
 void gradeFinder(){
     string SearchName;
     bool SearchCheck=false;
@@ -58,10 +129,6 @@ void gradeFinder(){
     cout<<endl;
 }
 
-/**
- * The classList function returns a list of all the students in a class
- * @param names - the array in which the students names are stored
- */
 void classList(){
     cout<<"Here is a list of all the students registered:"<<endl;
     for (int a = 0; a < s; a++) {
@@ -70,11 +137,6 @@ void classList(){
     cout<<endl;
 }
 
-/**
- * The classGrades function returns a list of all of the students in the class with their respective grades
- * @param names - the array in which the students names are stored
- * @param grades - the array in which the students grades are stored
- */
 void classGrades(){
     cout<<"Here is a list of all the students registered and their grade:"<<endl;
     for (int a = 0; a < s; a++) {
@@ -83,34 +145,68 @@ void classGrades(){
     cout<<endl;
 }
 
-/**
- * The main of this program is where the menu is located and runs from
- * @param prgm - prgm is the name given to the user input
- * the user input is the number that decides which function is run from the menu
- * after the selected function is run the menu will ask if the user would like to use another function
- * if the user input is yes, the menu will run once again from the beginning
- * if the user input is no, the program will exit
- * @param grade - the grade input for the student
- * @param name - the name input for the student
- * @param choice - the choice input is the first step of the program that allows the teacher to input the names and
- * grades of students
- * @param answer - the user input that decides if the user would like to continue using the program or if they would
- * like it to close
- */
+void gradeScale(){
+    cout<<"Which letter would you like to see the requirements for? (A/B/C/D/F)"<<endl;
+    cin>>letter;
+    if(letter == "A"){
+        Grade number;
+        number = A;
+        cout << "To recieve a(n) "<<letter<<" you need at least a(n) " << number;
+        cout<<endl;
+        cout<<endl;
+    }
+    if(letter == "B"){
+        Grade number;
+        number = B;
+        cout << "To recieve a(n) "<<letter<<" you need at least a(n) " << number;
+        cout<<endl;
+        cout<<endl;
+    }
+    if(letter == "C"){
+        Grade number;
+        number = C;
+        cout << "To recieve a(n) "<<letter<<" you need at least a(n) " << number;
+        cout<<endl;
+        cout<<endl;
+    }
+    if(letter == "D"){
+        Grade number;
+        number = D;
+        cout << "To recieve a(n) "<<letter<<" you need at least a(n) " << number;
+        cout<<endl;
+        cout<<endl;
+    }
+    if(letter == "F"){
+        Grade number;
+        number = F;
+        cout << "To recieve a(n) "<<letter<<" you need at least a(n) " << number;
+        cout<<endl;
+        cout<<endl;
+    }
+}
 
 int main() {
 
     int grade, choice, prgm;
-    string name, answer;
-
-    while (loop) {
-        cout << "Welcome to the Teacher Program! Press 1 to continue" << endl << endl;
-        cout << "1) The Teacher Program" << endl << endl;
-        cin >> prgm;
-        cout << "\n\n";
-        if (prgm == 1) {
-            for (int a = 0; a < s; a++) {
-
+    string name, firstName, lastName, answer, username, password;
+    Teacher userTeacher;
+    cout << "Please enter a username to be used: ";
+    cin >> username;
+    userTeacher.setUserName(username);
+    cout << "Please enter a password to be used: ";
+    cin >> password;
+    userTeacher.setPassword(password);
+    cout << "Now log in." <<endl;
+    userTeacher.login(userTeacher.getUserName(), userTeacher.getPassword());
+    if(userTeacher.isLoginCorrect()) {
+        while (loop) {
+            cout << "Welcome to the Teacher Program! Press 1 to continue" << endl << endl;
+            cout << "1) The Teacher Program" << endl << endl;
+            cin >> prgm;
+            cout << "\n\n";
+            if (prgm == 1) {
+                for (int a = 0; a < s; a++) {
+                
                 cout << "Input name" << "\n\n";
                 cin >> name;
                 cout << endl;
@@ -120,122 +216,46 @@ int main() {
                 cin >> grade;
                 cout << endl;
                 grades[a] = grade;
+                }
+                while (run) {
+                    cout << "Which application would you like to use?" << "\n\n";
+                    cout << "1) Class Stats" << "\n\n";
+                    cout << "2) Grade Finder" << "\n\n";
+                    cout << "3) Class List" << "\n\n";
+                    cout << "4) Class list with grades" << "\n\n";
+                    cout << "5) Display Grade Scale" << "\n\n";
+                    cin >> choice;
+                    cout << "\n\n";
+                    if (choice == 1) {
+                        stats();
+                    }
+                    if (choice == 2) {
+                        gradeFinder();
+                    }
+                    if (choice == 3) {
+                        classList();
+                    }
+                    if (choice == 4) {
+                        classGrades();
+                    }
+                    if (choice == 5) {
+                        gradeScale();
+                    }
+                    run = true;
+                    cout << "Would you like to run another application?" << endl;
+                    cin >> answer;
+                    cout << "\n\n";
+                    if (answer == "No" || answer == "no" || answer == "NO") {
+                        run = false;
+                        loop = false;
+                    }
+                }
+            } else {
+                return 0;
             }
-            while (run) {
-                cout << "Which application would you like to use?" << "\n\n";
-                cout << "1) Class Stats" << "\n\n";
-                cout << "2) Grade Finder" << "\n\n";
-                cout << "3) Class List" << "\n\n";
-                cout << "4) Class list with grades" << "\n\n";
-                cin >> choice;
-                cout << "\n\n";
-                if (choice == 1) {
-                    stats();
-                }
-                if (choice == 2) {
-                    gradeFinder();
-                }
-                if (choice == 3) {
-                    classList();
-                }
-                if (choice == 4) {
-                    classGrades();
-                }
-                run = true;
-                cout << "Would you like to run another application?" << endl;
-                cin >> answer;
-                cout << "\n\n";
-                if (answer == "No" || answer == "no" || answer == "NO" || answer == "nO") {
-                    run = false;
-                    loop=false;
-                }
-            }
-        }
-        else {
-            return 0;
         }
     }
 
     system("pause");
     return 0;
 }
-/**
- * The student class holds the name of the student as well as the first and last name of the student as separate strings
- * @param name - string holding the first and last name of the student
- * @param firstName - string holding the first name of the student
- * @param lastName - string holding the last name of the student
- * @param grade - int holding the number grade of the strudent
- */
-class student {
-private:
-    string name, firstName, lastName;
-    int grade;
-public:
-    // Function setFirstName is used to set the first name of a student
-    // @param nameOne - string set to hold the first name of the student before it is set to equal string firstName
-    void setFirstName(string nameOne){
-        firstName = nameOne;
-    }
-    // Function setLastName is used to set the last name of a student
-    // @param nameTwo - string set to hold the last name of the student before it is set to equal string lastName
-    void setLastName(string nameTwo){
-        lastName = nameTwo;
-    }
-    // Function setName is used to combine the strings of first name and last name into the full name of a student
-    // @param firstName - the first name of the student
-    // @param lastName - the last name of the student
-    // @param name - string holding the newly combined full name of a student
-    void setName(){
-        name = firstName + " " + lastName;
-    }
-    // Function setGrade assigns a number grade to a student
-    // @param studentGrade sets an integer to the student grade
-    void setGrade(int studentGrade){
-        grade = studentGrade;
-    }
-    // Function getName returns the name of a student
-    string getName(){
-        return name;
-    }
-    // Function getFirstName returns only the first name of the student
-    string getFirstName(){
-        return firstName;
-    }
-    // Function getLastName returns only the last name of the student
-    string getLastName() {
-        return lastName;
-    }
-    // Function getGrade returns the number grade of the student
-    int getGrade(){
-        return grade;
-    }
-};
-/**
- * The teacher class holds the credentials for the teacher that is using the program
- * @param userName - private string that holds the set username for the teacher
- * @param password - private string that holds the set password for the teacher
- */
-class teacher{
-private:
-    string userName, password;
-public:
-    // Function setUserName is used to set the username of the teacher
-    // @param teacherUserName is a string that will be set equal to the username of the teacher
-    void setUserName(string teacherUserName){
-        userName = teacherUserName;
-    }
-    // Function setPassword is used to set the password for the teacher
-    //@param teacherPassword is a string that will be set equal to the username of the teacher
-    void setPassword(string teacherPassword){
-        password = teacherPassword;
-    }
-    // Function getUserName returns the set username of the teacher
-    string getUserName(){
-        return userName;
-    }
-    // Function getPassword returns the set password of the teacher
-    string getPassword(){
-        return password;
-    }
-
-};
